c303e8a82672e25f09c9b1637e0921112dacac76
Magno Vicentini <magno-vicentini@hotmail.com>
2021-02-04 22:03:46 -0300

    navegacaofinal

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,30 @@
+import './Menu.css'
+import React from 'react'
+
+import {Link}from 'react-router-dom'
+
+const Menu = props => (
+<div className="Menu">
+    <nav>
+        <ul>
+            <li>
+                <Link to='/'>Início</Link>
+            </li>
+            <li>
+                <Link to='/param/123'>Param #01</Link>
+            </li>
+            <li>
+                <Link to='/param/legal'>Param #02</Link>
+            </li>
+            <li>
+                <Link to='/About'>Sobre</Link>
+            </li>
+            <li>
+                <Link to='/NotFound'>Não existe!</Link>
+            </li>
+        </ul>
+    </nav>
+</div>
+)
+
+export default Menu
